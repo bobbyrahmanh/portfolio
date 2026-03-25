3ca00c8a93a500c1f6c37a74a559db964bd21534
adamafriansyahb <adamafriansyahb@gmail.com>
2026-03-25 11:05:13 +0700

    feat: final backend config for deployment

--- backend/config/middlewares.js
+++ backend/config/middlewares.js
@@ -1,11 +1,36 @@
+// config/middlewares.js
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
-  'strapi::body',
+  // BAGIAN INI YANG DIUBAH:
+  {
+    name: 'strapi::body',
+    config: {
+      formLimit: '256mb', // Naikin limit form
+      jsonLimit: '256mb', // Naikin limit JSON
+      textLimit: '256mb',
+      formidable: {
+        maxFileSize: 200 * 1024 * 1024, // Sekitar 200MB
+      },
+    },
+  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
